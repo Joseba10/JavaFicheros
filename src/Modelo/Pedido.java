1d242265a4d250e929b181584c741fcfa817789d
J <joseba.3.1.3@hotmail.com>
2019-05-30 00:42:54 +0200

    Añadido toString

--- src/Modelo/Pedido.java
+++ src/Modelo/Pedido.java
@@ -1,39 +1,47 @@
 package Modelo;
 
 public class Pedido {
-	
+
 	private int codPedido;
 	private Cliente codCliente;
 	private double detalle;
-	
-	
+
 	public Pedido(int codPedido, Cliente codCliente, double detalle) {
-	
+
 		this.codPedido = codPedido;
 		this.codCliente = codCliente;
 		this.detalle = detalle;
 	}
-	
-	//Metodos Getter y Setters
+
+	// Metodos Getter y Setters
+
 	public int getCodPedido() {
 		return codPedido;
 	}
+
 	public void setCodPedido(int codPedido) {
 		this.codPedido = codPedido;
 	}
+
 	public Cliente getCodCliente() {
 		return codCliente;
 	}
+
 	public void setCodCliente(Cliente codCliente) {
 		this.codCliente = codCliente;
 	}
+
 	public double getDetalle() {
 		return detalle;
 	}
+
+	@Override
+	public String toString() {
+		return "Pedido [codPedido=" + codPedido + ", codCliente=" + codCliente + ", detalle=" + detalle + "]";
+	}
+
 	public void setDetalle(double detalle) {
 		this.detalle = detalle;
 	}
-	
-	
 
 }
